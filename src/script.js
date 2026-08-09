46e1c1bcd767c65599c7b622128d8d30e1abdfb2
Miriam Kilangi <miriamkilangi@outlook.com>
2026-08-09 16:02:51 +0300

    injecting images in the url api

--- src/script.js
+++ src/script.js
@@ -7,6 +7,8 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
   timeElement.innerHTML = formatDate(date);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
